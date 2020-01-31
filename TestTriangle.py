6dd62735b318612052e4f9a3f0e91ac7f6df4b7e
Varun-Mullins <57276726+Varun-Mullins@users.noreply.github.com>
2020-01-31 14:35:12 -0500

    Update TestTriangle.py 2

--- TestTriangle.py
+++ TestTriangle.py
@@ -18,41 +18,39 @@ class TestTriangles(unittest.TestCase):
      """Tests for checking the Triangle file"""
 
     def testValid1(self):
-        """Test for input above 200"""
+        #Test for input above 200
         self.assertEqual(classifyTriangle(201, 0, 4), 'InvalidInput')
 
     def testValid2(self):
-        """Test for input less than or equal to 0"""
+        #Test for input less than or equal to 0
         self.assertEqual(classifyTriangle(-1, 0, 6), 'InvalidInput')
 
     def testValidInp(self):
-        """Test for input not an integer"""
+        #Test for input not an integer
         self.assertEqual(classifyTriangle('a', 'b', 2), 'InvalidInput')
 
     def triangleTest(self):
-        """Test to check the validity of the triangle"""
+        #Test to check the validity of the triangle
         self.assertEqual(classifyTriangle(1, 10, 12), 'NotATriangle')
 
     def testRightTriangleA(self):
-        """Test if the triangle is a right angle Triangle"""
-
+        #Test if the triangle is a right angle Triangle
         self.assertEqual(classifyTriangle(3,4,5),'Right')
 
     def testRightTriangleB(self): 
-        """Test if the triangle is a right angle Triangle"""
-
+        #Test if the triangle is a right angle Triangle
         self.assertEqual(classifyTriangle(5,3,4),'Right')
         
     def testEquilateralTriangles(self): 
-        """Test if the triangle is an Equilateral Triangle"""        
+        #Test if the triangle is an Equilateral Triangle        
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
         
     def testIsoscelesTriangles(self):
-        """Test if the triangle is an Isosceles Triangle"""
+        #Test if the triangle is an Isosceles Triangle
         self.assertEqual(classifyTriangle(1, 3, 3), 'Isoceles')
 
     def testScaleneTriangles(self):
-        """Test if the triangle is a Scalene Triangle"""
+        #Test if the triangle is a Scalene Triangle
         self.assertEqual(classifyTriangle(1, 3, 4), 'Scalene')
 
 if __name__ == '__main__':
