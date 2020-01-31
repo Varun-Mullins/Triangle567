1ee25bc200478b0ed36176d42b053914ccfe6614
Varun-Mullins <57276726+Varun-Mullins@users.noreply.github.com>
2020-01-31 14:37:47 -0500

    Update TestTriangle.py

--- TestTriangle.py
+++ TestTriangle.py
@@ -16,40 +16,40 @@ from Triangle import classifyTriangle
 
 class TestTriangles(unittest.TestCase):
      """Tests for checking the Triangle file"""
-
-    def testValid1(self):
+     
+     def testValid1(self):
         #Test for input above 200
         self.assertEqual(classifyTriangle(201, 0, 4), 'InvalidInput')
-
-    def testValid2(self):
+     
+     def testValid2(self):
         #Test for input less than or equal to 0
         self.assertEqual(classifyTriangle(-1, 0, 6), 'InvalidInput')
 
-    def testValidInp(self):
+     def testValidInp(self):
         #Test for input not an integer
         self.assertEqual(classifyTriangle('a', 'b', 2), 'InvalidInput')
 
-    def triangleTest(self):
+     def triangleTest(self):
         #Test to check the validity of the triangle
         self.assertEqual(classifyTriangle(1, 10, 12), 'NotATriangle')
 
-    def testRightTriangleA(self):
+     def testRightTriangleA(self):
         #Test if the triangle is a right angle Triangle
         self.assertEqual(classifyTriangle(3,4,5),'Right')
 
-    def testRightTriangleB(self): 
+     def testRightTriangleB(self): 
         #Test if the triangle is a right angle Triangle
         self.assertEqual(classifyTriangle(5,3,4),'Right')
         
-    def testEquilateralTriangles(self): 
+     def testEquilateralTriangles(self): 
         #Test if the triangle is an Equilateral Triangle        
         self.assertEqual(classifyTriangle(1,1,1),'Equilateral')
         
-    def testIsoscelesTriangles(self):
+     def testIsoscelesTriangles(self):
         #Test if the triangle is an Isosceles Triangle
         self.assertEqual(classifyTriangle(1, 3, 3), 'Isoceles')
 
-    def testScaleneTriangles(self):
+     def testScaleneTriangles(self):
         #Test if the triangle is a Scalene Triangle
         self.assertEqual(classifyTriangle(1, 3, 4), 'Scalene')
 
